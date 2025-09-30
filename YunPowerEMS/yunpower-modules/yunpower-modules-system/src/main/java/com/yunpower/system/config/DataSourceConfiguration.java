9b46c7c0d820c8fde0e0b74c5dc9781201b44732
yanhl <18729041176@163.com>
2025-09-30 16:56:49 +0800

    fix upgrade bug

---
+++ YunPowerEMS/yunpower-modules/yunpower-modules-system/src/main/java/com/yunpower/system/config/DataSourceConfiguration.java
@@ -0,0 +1,67 @@
+package com.yunpower.system.config;
+
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
+import com.baomidou.dynamic.datasource.provider.AbstractDataSourceProvider;
+import com.baomidou.dynamic.datasource.provider.DynamicDataSourceProvider;
+import com.baomidou.dynamic.datasource.creator.DataSourceProperty;
+import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties;
+import com.baomidou.dynamic.datasource.creator.DefaultDataSourceCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+import java.util.Map;
+
+/**
+ * 动态数据源配置
+ * 
+ * @author yunpower
+ */
+@Configuration
+public class DataSourceConfiguration {
+
+    /**
+     * 动态数据源配置项
+     */
+    @Autowired
+    private DynamicDataSourceProperties properties;
+
+    /**
+     * 数据源创建器
+     */
+    @Autowired
+    private DefaultDataSourceCreator dataSourceCreator;
+
+    /**
+     * 数据源提供者
+     */
+    @Bean
+    public DynamicDataSourceProvider dynamicDataSourceProvider() {
+        Map<String, DataSourceProperty> datasourceMap = properties.getDatasource();
+        return new AbstractDataSourceProvider(dataSourceCreator) {
+            @Override
+            public Map<String, DataSource> loadDataSources() {
+                return createDataSourceMap(datasourceMap);
+            }
+        };
+    }
+
+    /**
+     * 将动态数据源设置为首选的
+     * 当spring存在多个数据源时, 自动注入的是首选的对象
+     */
+    @Primary
+    @Bean
+    public DataSource dataSource() {
+        DynamicRoutingDataSource dataSource = new DynamicRoutingDataSource(java.util.Collections.singletonList(dynamicDataSourceProvider()));
+        dataSource.setPrimary(properties.getPrimary());
+        dataSource.setStrict(properties.getStrict());
+        dataSource.setStrategy(properties.getStrategy());
+        dataSource.setP6spy(properties.getP6spy());
+        dataSource.setSeata(properties.getSeata());
+        return dataSource;
+    }
+}
